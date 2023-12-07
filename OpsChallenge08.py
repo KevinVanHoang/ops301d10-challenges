9b77139bee8ada44fb7b9f1e879dd01e39234149
KevinVanHoang <hoang.kevin67@gmail.com>
2023-12-06 23:27:18 -0800

    Add OpsChallenge08.py

---
+++ OpsChallenge08.py
@@ -0,0 +1,28 @@
+# Python
+
+# Script Name:                  Ops Challenge: Python Collections
+# Author:                       Kevin Hoang
+# Date of latest revision:      12/06/2023
+# Purpose:                      Python script works with lists 
+# Execution:                    OpsChallenge08.py
+# Resource:                     https://chat.openai.com/c/b4ad148f-8c34-4e54-95ff-464297a23b32
+#                               https://www.w3schools.com/python/gloss_python_assign_string_variable.asp
+#                               https://www.w3schools.com/python/python_lists_access.asp
+#                               https://github.com/codefellows/seattle-ops-301d10/tree/main/class-08/challenges
+#                               https://github.com/codefellows/seattle-ops-301d10/blob/main/class-08/demo.py
+
+
+# Assigning a list of ten string elements to a variable
+my_list = ["element1", "element2", "element3", "element4", "element5", "element6", "element7", "element8", "element9", "element10"]
+
+# Print the fourth element of the list
+print(my_list[3])
+
+# Print the sixth through tenth elements of the list
+print(my_list[5:10])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Print the updated list
+print(my_list)
